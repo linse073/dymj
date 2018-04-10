41bfc483d954565d87f95d992013a5fa7aa5bc93
linse073 <linse073@gmail.com>
2018-04-10 12:49:34 +0800

    test boradcast

--- service/gmd.lua
+++ service/gmd.lua
@@ -18,7 +18,7 @@ local CMD = {}
 function CMD.test_broadcast()
     local master = skynet.queryservice("mongo_master")
     local user_db = skynet.call(master, "lua", "get", "user")
-    local cursor = skynet.call(user_db, "lua", "find", nil, {id=true})
+    local cursor = skynet.call(user_db, "lua", "find", {id=1811})
     while cursor:hasNext() do
         local r = cursor:next()
         util.dump(r)
